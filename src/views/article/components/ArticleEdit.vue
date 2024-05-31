e46d6becfc07597b551a8369f26e35c4266d97d7
乐天居易 <19836788252@163.com>
2024-05-31 09:32:47 +0800

    备份

--- src/views/article/components/ArticleEdit.vue
+++ src/views/article/components/ArticleEdit.vue
@@ -3,105 +3,128 @@ import { ref } from 'vue'
 import ChannelSelect from './ChannelSelect.vue'
 import { Plus } from '@element-plus/icons-vue'
 import { QuillEditor } from '@vueup/vue-quill'
-import { baseURL } from '@/utils/request'
+import '@vueup/vue-quill/dist/vue-quill.snow.css'
 import {
   artPublishService,
   artGetDetailService,
   artEditService
 } from '@/api/article'
-import '@vueup/vue-quill/dist/vue-quill.snow.css'
-import { ElMessage } from 'element-plus'
+import { baseURL } from '@/utils/request'
 import axios from 'axios'
+// 控制抽屉显示隐藏
 const visibleDrawer = ref(false)
+
+// 默认数据
 const defaultForm = {
-  title: '',
-  cate_id: '',
-  cover_img: '',
-  content: '',
-  state: ''
+  title: '', // 标题
+  cate_id: '', // 分类id
+  cover_img: '', // 封面图片 file 对象
+  content: '', // string 内容
+  state: '' // 状态
 }
+
+// 准备数据
 const formModel = ref({ ...defaultForm })
 
+// 图片上传相关逻辑
 const imgUrl = ref('')
-const onUploadFile = (uploadFile) => {
-  // 进行图片预览
-  imgUrl.value = URL.createObjectURL(uploadFile.raw)
-  // 保存数据准备提交
+const onSelectFile = (uploadFile) => {
+  imgUrl.value = URL.createObjectURL(uploadFile.raw) // 预览图片
+  // 立刻将图片对象，存入 formModel.value.cover_img 将来用于提交
   formModel.value.cover_img = uploadFile.raw
 }
+
+// 提交
 const emit = defineEmits(['success'])
-const quillEditor = ref()
 const onPublish = async (state) => {
-  // 将已发布还是草稿状态，存入 state
+  // 将已发布还是草稿状态，存入 formModel
   formModel.value.state = state
-  // 转换 formData 数据
+
+  // 注意：当前接口，需要的是 formData 对象
+  // 将普通对象 => 转换成 => formData对象
   const fd = new FormData()
   for (let key in formModel.value) {
     fd.append(key, formModel.value[key])
   }
-  if (formModel.value.cate_id) {
-    console.log('编辑操作')
+
+  // 发请求
+  if (formModel.value.id) {
+    // 编辑操作
     await artEditService(fd)
-    ElMessage.success('编辑成功')
+    ElMessage.success('修改成功')
     visibleDrawer.value = false
     emit('success', 'edit')
   } else {
+    // 添加操作
     await artPublishService(fd)
-    ElMessage.success('发布成功')
+    ElMessage.success('添加成功')
     visibleDrawer.value = false
-    // 添加完成后的内容重置
-    imgUrl.value = ''
-    quillEditor.value.setHTML('')
+    // 通知到父组件，添加成功了
     emit('success', 'add')
   }
 }
+
+// 组件对外暴露一个方法 open，基于open传来的参数，区分添加还是编辑
+// open({})  => 表单无需渲染，说明是添加
+// open({ id, ..., ... })  => 表单需要渲染，说明是编辑
+// open调用后，可以打开抽屉
+const editorRef = ref()
 const open = async (row) => {
-  visibleDrawer.value = true
+  visibleDrawer.value = true // 显示抽屉
+
   if (row.id) {
-    console.log('编辑回显')
+    // 需要基于 row.id 发送请求，获取编辑对应的详情数据，进行回显
     const res = await artGetDetailService(row.id)
-    formModel.value.id = row.id
     formModel.value = res.data.data
-    // shu'j
+    // 图片需要单独处理回显
     imgUrl.value = baseURL + formModel.value.cover_img
-    // 提交给后台，需要的是 file 格式的，将网络图片，转成 file 格式
-    // 网络图片转成 file 对象, 需要转换一下
-    const file = await imageUrlToFile(imgUrl.value, 'cover_img')
+    // 注意：提交给后台，需要的数据格式，是file对象格式
+    // 需要将网络图片地址 => 转换成 file对象，存储起来, 将来便于提交
+    const file = await imageUrlToFileObject(
+      imgUrl.value,
+      formModel.value.cover_img
+    )
     formModel.value.cover_img = file
   } else {
-    formModel.value = { ...defaultForm }
-    console.log('添加功能')
+    formModel.value = { ...defaultForm } // 基于默认的数据，重置form数据
+    // 这里重置了表单的数据，但是图片上传img地址，富文本编辑器内容 => 需要手动重置
+    imgUrl.value = ''
+    editorRef.value.setHTML('')
   }
 }
-// 将网络图片地址转换为File对象
-async function imageUrlToFile(url, fileName) {
-  try {
-    // 第一步：使用axios获取网络图片数据
-    const response = await axios.get(url, { responseType: 'arraybuffer' })
-    const imageData = response.data
 
-    // 第二步：将图片数据转换为Blob对象
-    const blob = new Blob([imageData], {
+// 将网络图片地址转换为 File 对象的函数
+async function imageUrlToFileObject(imageUrl, filename) {
+  try {
+    // 使用 Axios 下载图片数据
+    const response = await axios.get(imageUrl, { responseType: 'arraybuffer' })
+
+    // 将下载的数据转换成 Blob 对象
+    const blob = new Blob([response.data], {
       type: response.headers['content-type']
     })
 
-    // 第三步：创建一个新的File对象
-    const file = new File([blob], fileName, { type: blob.type })
+    // 创建 File 对象
+    const file = new File([blob], filename, {
+      type: response.headers['content-type']
+    })
 
     return file
   } catch (error) {
-    console.error('将图片转换为File对象时发生错误:', error)
-    throw error
+    console.error('Error converting image URL to File object:', error)
+    return null
   }
 }
-defineExpose({ open })
+
+defineExpose({
+  open
+})
 </script>
 
 <template>
-  <!-- 抽屉 -->
   <el-drawer
     v-model="visibleDrawer"
-    :title="formModel.cate_id ? '编辑教材' : '添加教材'"
+    :title="formModel.id ? '编辑教材' : '添加教材'"
     direction="rtl"
     size="50%"
   >
@@ -117,26 +140,28 @@ defineExpose({ open })
         ></channel-select>
       </el-form-item>
       <el-form-item label="教材封面" prop="cover_img">
+        <!-- 此处需要关闭 element-plus 的自动上传，不需要配置 action 等参数
+             只需要做前端的本地预览图片即可，无需在提交前上传图标
+             语法：URL.createObjectURL(...) 创建本地预览的地址，来预览
+        -->
         <el-upload
           class="avatar-uploader"
-          :auto-upload="false"
-          :on-change="onUploadFile"
           :show-file-list="false"
+          :auto-upload="false"
+          :on-change="onSelectFile"
         >
           <img v-if="imgUrl" :src="imgUrl" class="avatar" />
-          <el-icon v-else class="avatar-uploader-icon">
-            <Plus />
-          </el-icon>
+          <el-icon v-else class="avatar-uploader-icon"><Plus /></el-icon>
         </el-upload>
       </el-form-item>
-      <el-form-item label="教材内容概括" prop="content">
+      <el-form-item label="教材内容概要" prop="content">
         <div class="editor">
-          <QuillEditor
+          <quill-editor
+            ref="editorRef"
             v-model:content="formModel.content"
+            content-type="html"
             theme="snow"
-            contentType="html"
-            ref="quillEditor"
-          />
+          ></quill-editor>
         </div>
       </el-form-item>
       <el-form-item>
@@ -146,6 +171,7 @@ defineExpose({ open })
     </el-form>
   </el-drawer>
 </template>
+
 <style lang="scss" scoped>
 .avatar-uploader {
   :deep() {
@@ -174,6 +200,7 @@ defineExpose({ open })
     }
   }
 }
+
 .editor {
   width: 100%;
   :deep(.ql-editor) {
